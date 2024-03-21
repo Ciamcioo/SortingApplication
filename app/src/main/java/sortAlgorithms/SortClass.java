be08bf459d3b0578a71d0b02025edf2df4a55a84
Ciamcio <jakub.maciocha@gmail.com>
2024-03-21 21:03:14 +0100

    Update to SortClass

--- app/src/main/java/sortAlgorithms/SortClass.java
+++ app/src/main/java/sortAlgorithms/SortClass.java
@@ -1,62 +1,82 @@
 package sortAlgorithms;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Random;
 
 public abstract class SortClass {
     protected int size = 0;
-    protected int[] array;
-    protected String fileName;
-    protected FileReader dataFile;
+    protected int[] array = null; 
+    protected String inputDataFailName;
     protected FileOutputStream resultFile;
 
     /**
-     * Constructor which as a argument take fileName from user, based on the fileName establishes conection with the fileName,
-     * reads data from the file and closes the conections with the file. 
-     * @param fileName - provided by the user
-     * @throws Exception 
+     * Constructor which as a argument take fileName from user, calls function to set variable of the object and read data from the file 
+     * @param fileName - name of the file which from data should be read 
+     * @throws Exception whenever the fileName is empty or file  
+     * @throws FileNotFound whenever file which name was specified do not exist
      */
     protected SortClass(String fileName) throws Exception {
         setFileName(fileName);
-        setDataFile();
         readDataFromFile();
     }
 
 
     /**
-    *  Constructor whicha as a agrument takes size of wanted array, creates the Array and generates data for it.
+    *  Constructor whicha as a agrument takes size of array, then sets the size, creates an array and generates data for it.
     * @param size
     */
     protected SortClass(int size) {
         setSize(size);
-        createArray();
+        array = createArray();
         generateDataForArray();
     }
 
     /**
-     *  Method is reading the data from file and inserting it into array variable
+     *  Method is reading data from file and inserting it into array as variable of next memory blocks 
      * @throws IOException
      */
-    private void readDataFromFile() throws IOException {
-        BufferedReader reader  = new BufferedReader(dataFile);
-        setSize(Integer.parseInt(reader.readLine()));
-        createArray();
-        for (int i=0; reader.ready() && i < size; i++) 
-            array[i] = Integer.parseInt(reader.readLine());
-        reader.close();
+    private void readDataFromFile()  {
+        BufferedReader reader = generatBufferedReader();
+        int i = -1;
+        try {
+            for (; reader.ready() && i < size; i++) { 
+                if (array == null) {
+                    setSize(Integer.parseInt(reader.readLine()));
+                    array = createArray();
+                }
+                array[i] = Integer.parseInt(reader.readLine());
+            }
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Problem with reading data from file");
+            System.out.println("Reader has stoped on line number: " + i);
+        } finally{
+            closeStream(reader);
+        }
     }
 
+    private static void closeStream(Closeable stream) {
+        try { 
+           stream.close(); 
+        } catch(IOException e) {
+            System.out.println("Problem with closing stream");
+            System.out.println("Stream type: " + stream.getClass());
+        } 
+    }
+
+    private BufferedReader generatBufferedReader() {
+        try {
+            return new BufferedReader(new FileReader(inputDataFailName));
+        } catch(FileNotFoundException e) {
+            return null;
+        }
+
+    }
     protected abstract void sortAlgorithm(); 
 
     /**
      * Method allocates data for array     
      */
-    protected void createArray() {
-        array = new int[size];
+    protected int[] createArray() {
+        return new int[size];
     }
 
     /**
@@ -82,23 +102,13 @@ public abstract class SortClass {
     }
 
     protected String getFileName() {
-        return fileName;
+        return inputDataFailName;
     }
 
     protected void setFileName(String fileName) throws Exception {
         if (!fileName.equals(""))
-            this.fileName = fileName;
+            inputDataFailName = fileName;
         else
             throw new Exception("Empty file name");
     }
-
-    public FileReader getDataFile() {
-        return dataFile;
-    }
-
-    public void setDataFile() throws FileNotFoundException {
-        this.dataFile = new FileReader(fileName);
-    }
-
-    
 }
